b0bbfa50a8f4535a6f259d88c5230d8cbe56457b
thescribe11 <thescribe11@gmail.com>
2021-05-18 14:36:13 -0500

    Added constants

--- src/engine.rs
+++ src/engine.rs
@@ -1,4 +1,7 @@
 const ROW: usize = 8;
+const BLACK: u8 = 0; const WHITE: u8 = 1;  // Convenience constants for white and black's turns.
+const scoresheet: [f32; 6] = [1.0, 3.5, 3.5, 5.25, 10.0, 200.0];
+
 
 pub struct State {
     pub board: [i8; 64],
@@ -46,7 +49,17 @@ impl State {
         }
     }
 
-    fn move_piece(&self, start_location: usize, end_location: usize) -> Option<State> {
+    fn get_square(&self, position: usize) -> Option<i8> {
+        //! Get if a square is occupied. Returns Option::None if not a valid square.
+
+        if position >=0 && position < 64 {
+            return Some(self.board[position])
+        } else {
+            return None
+        }
+    }
+
+    fn move_piece(&self, start_location: &usize, end_location: &usize) -> State {
         //! Moves a piece to a target square provided that the square is valid.
         //! 
         //! Arguments:
@@ -55,17 +68,42 @@ impl State {
         
         let mut result: State = self.copy();
 
-        if end_location >= 0 && end_location < 65 {
-            let target = self.board[end_location];
+        let piece = result.board[*start_location];
+        result.board[*start_location] = 0;
 
-            
+        if end_location >= &0 && end_location < &65 {
+            result.score += scoresheet[result.board[*end_location].abs() as usize];
+            result.board[*end_location] = piece;
         }
 
-        return Some(result)
+        result
     }
+
+
+    fn gen_moves(&self) -> State {
+        let mut moves_tree: Vec<State> = Vec::new();
+
+        if self.depth % 2 == 0 {  // It's black's turn. I'm generating moves with the assumption that black goes first, since black is the AI.
+            for piece in &self.board {
+                
+            }
+        }
+        else {
+            for piece in &self.board {
+
+            }
+        }
+
+        unimplemented!()
+    }
+
 
     fn move_b_pawn(&self, position: &usize) -> Vec<State> {
         let mut to_return: Vec<State> = Vec::new();  // This is basically a placeholder.
+
+        if self.get_square(position + ROW) >= 0 {
+            to_return.push(self.move_piece(position, &(position + ROW)));
+        }
 
         return to_return
     }
